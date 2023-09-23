8c4fe524a26e75c1785aa97f6fa73ab99abc281f
bijan <bmassoumi@gmail.com>
2023-09-23 13:44:44 -0400

    add deploy

---
+++ script/deployLocal.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "../src/ShareSample.sol";
+import "forge-std/Script.sol";
+
+contract DeployLocal is Script {
+    ShareSample keys;
+    uint256 oneETH = 10 ** 18;
+    address owner = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address pub1 = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
+    address pub2 = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
+    address pub3 = 0x90F79bf6EB2c4f870365E785982E1f101E93b906;
+    uint256 feeRate = 2000;
+
+    function run() public {
+        uint256 ownerpk = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(ownerpk);
+        keys = new ShareSample(owner, feeRate, owner);
+    }
+}
